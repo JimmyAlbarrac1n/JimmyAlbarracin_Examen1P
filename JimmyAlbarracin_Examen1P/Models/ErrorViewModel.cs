326efe520e7afc92fa88dcfeca44fc0908c16f3e
laboratorio <laboratorio@UPOAULA10303>
2024-04-24 10:29:12 -0500

    Agregar archivos de proyecto.

---
+++ JimmyAlbarracin_Examen1P/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JimmyAlbarracin_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
